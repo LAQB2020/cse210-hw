67fabf26dbc2965bab5e092dd5cb8981d59a99c8
LAQB2020 <luisquezadaangel@gmail.com>
2026-02-28 17:31:57 -0600

    Firts revision of example 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        int grades;     
+        string input, letter; 
+
+            
+        //loop to validate user´s input be a number between 0 and 100    
+        while(true)
+        {
+            
+            Console.Write("What is your grade? ");
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out grades) && grades >= 0 && grades <= 100)
+                {
+                    break;
+                }
+
+            else
+                {
+                Console.WriteLine("Invalid input. Enter a number between 0 and 100.");   
+                }
+        }
+        
+        // if else statements to determine the final letter grade 
+        if(grades >= 90 )
+        {
+            letter = "A";
+        }
+        
+        else if (grades >= 80)
+        {
+            letter = "B";
+        }
+        
+        else if (grades >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grades >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+        
+        // determinating the letter sign
+        int gradeSign;
+        string letterSign= " ";
+
+        gradeSign = grades % 10;
+
+        if (gradeSign >= 7)
+        {
+            letterSign = "+";
+        }
+        else if (gradeSign <= 3)
+        {
+            letterSign = "-";
+        }
+
+        //Printing results
+        Console.Write($"Your grade letter is {letterSign}{grades} ");
+
+        if (grades >= 60 )
+        {
+            Console.Write("Congratulations!!!!!");
+        }
+
+        else
+        {
+            Console.Write("You can do better next time. Go ahead!");
+        }
+
+
     }
 }
