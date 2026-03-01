3e3ca8ee302ceaa073c0276844b4f6993e272536
LAQB2020 <luisquezadaangel@gmail.com>
2026-02-28 18:25:56 -0600

    First revision

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+        do {
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 100), userNumber;    
+            do
+            {
+                while (true)
+                {
+                Console.WriteLine("Whats is your guess? ");
+                string userInput = Console.ReadLine();
+                    if (int.TryParse(userInput, out userNumber) && userNumber <= 100 )
+                    {
+                    break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please type a number between 1 to 100");
+                    }
+                }
+            
+                if (userNumber > randomNumber) Console.WriteLine("Lower");
+                else if (userNumber < randomNumber) Console.WriteLine("Higer");
+                else Console.WriteLine("You guess it");     
+
+            }while(userNumber!=randomNumber);
+            
+            Console.WriteLine("Play again? yes/no");
+            playAgain = Console.ReadLine();
+
+        }while(playAgain =="yes");
+                   
     }
 }
