aaf1d6f208992e06e6a9e4e0e36c2f445172745a
LAQB2020 <luisquezadaangel@gmail.com>
2026-04-07 20:44:42 -0600

    Initial Issue

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        
+        
+        
+        
+        Square s1 = new Square("white",5);
+        Rectangle r1 = new Rectangle("blue", 5, 4 );
+        Circle c1 = new Circle ("orange",3 );
+
+        List<Shape> list = new List<Shape>();
+
+        list.Add(s1);
+        list.Add(r1);
+        list.Add(c1);
+
+        foreach (var shape in list)
+        {
+            Console.WriteLine($"The figure color {shape.GetColor()} has an area equal to {shape.GetArea()} units ^ 2");
+        }
+
+        
     }
 }
